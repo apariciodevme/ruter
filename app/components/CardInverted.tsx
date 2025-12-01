0c29c77e54031c873596d5986f6e269b8f10815c
apariciodevme <apariciodev@proton.me>
2025-12-01 12:47:57 +0100

    cardInverted component

--- app/components/CardInverted.tsx
+++ app/components/CardInverted.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 
-const Card = ({text, title, img, btnText}: Props) => {
+const CardInverted = ({text, title, img, btnText}: Props) => {
   return (
     <div className='flex relative pb-5 hover:text-button flex-col mt-5 cursor-pointer hover: rounded-3xl bg-secondary items-center justify-center gap-3 m-7 transition duration-300  hover:bg-[#3d3e45]  '>
       <Image
@@ -22,8 +22,8 @@ const Card = ({text, title, img, btnText}: Props) => {
  
       {/*Text and Button*/}
       <div className='flex flex-col items-center text-center justify-center p-5 gap-2 h-full '>
-      <h1 className='text-xl font-semibold'>{title}</h1>
       <p className='text-md text-gray-400 text-center max-w-lg mx-4'>{text}</p>
+      <h1 className='text-xl font-semibold'>{title}</h1>
       <button className='px-4 mt-2 flex cursor-pointer items-center gap-2 py-1.5 bg-button/20 text-[#86AFFF] rounded-full bg text-md hover:bg-button/30 transition duration-200'>{btnText}</button>
       </div>
    
@@ -31,4 +31,4 @@ const Card = ({text, title, img, btnText}: Props) => {
   )
 }
 
-export default Card
+export default CardInverted
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,31 @@ import Image from "next/image";
 import SearchBox from "./components/SearchBox";
 import Stack from "./components/Stack";
 import Card from "./components/Card";
+import CardInverted from "./components/CardInverted";
 
 export default function Home() {
   return (
    <div className="flex flex-col">
     <SearchBox/>
     <Stack/>
-    <Card title="Night-time metro from Saturday-Sunday" text="From November 16thto December 28th, you can take the metro, night to sunday on lines 2 and 3." img="" btnText="More information"/>
+
+
+
+    <Card title="Night-time metro from Saturday-Sunday" text="From November 16thto December 28th, you can take the metro, night to sunday on lines 2 and 3." img="/1.jpg" btnText="More information"/>
+
+    <Card title="Large overview of Christmas events" text="How to get to the Christmas markets without a car" img="/2.jpg" btnText="Get travel advice"/>
+
+
+  <CardInverted text="The work on the metro continues" title="Stortinget station is closed until December 2nd" img="/3.jpg" btnText="More information"/>
+
+
+  <CardInverted text="New rules from July 1st" title="Free public transport for asylum seekers" img="/3.jpg" btnText="More information"/>
+
+
+  <CardInverted text="Winter is right around the corner" title="We have made these improvements" img="/3.jpg" btnText="More information"/>
+
+
+  
    </div>
   );
 }
